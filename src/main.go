63999f1273c60d0a7d05e248e7338e21b90a107b
emolina82 <49229835+emolina82@users.noreply.github.com>
2022-02-03 19:11:12 +0100

    Docker

--- src/main.go
+++ src/main.go
@@ -12,14 +12,14 @@ func main() {
 
 	router := gin.Default()
 
-	router.SetTrustedProxies([]string{"1.1.1.1"})
+	//router.SetTrustedProxies([]string{"1.1.1.1"})
 
 	router.GET("/ip", service.GetIps)
 	router.GET("/ip/:id", service.GetIpById)
 	router.POST("/ip", service.PostIp)
 	router.DELETE("/ip/:id", service.DeleteIpById)
 
-	router.Run("localhost:8080")
+	router.Run("0.0.0.0:8080")
 
 	fmt.Println("Finishing")
 }
